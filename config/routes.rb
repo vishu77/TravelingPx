a843c0cbc02987122de769c978698ad59197998d
Eric <efeng288@gmail.com>
2016-08-06 11:02:13 -0400

    fixed routes that was causing images to not load up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#root'
-  get "/*path", to: 'static_pages#root'
-
-
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource  :session, only: [:create, :destroy, :show]
     resources :photos, only: [:create, :destroy, :index, :show, :update]
   end
 
+  root to: 'static_pages#root'
+  get "/*path", to: 'static_pages#root'
 end
