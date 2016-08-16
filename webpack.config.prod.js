7ed2bd01600d7f9efafaf22ed9e00b74729ac7d8
Eric <efeng288@gmail.com>
2016-08-16 19:08:59 -0400

    minified copy of react

---
+++ webpack.config.prod.js
@@ -0,0 +1,46 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/travelingpx.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /masonry|imagesloaded|fizzy\-ui\-utils|desandro\-|outlayer|get\-size|doc\-ready|eventie|eventemitter/,
+        loader: 'imports?define=>false&this=>window'
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
